65242be286fb809f9955a061009bc2e9155e0189
Weston Platter <westonplatter@gmail.com>
2016-12-29 14:27:43 -0700

    install devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # just so it has a config. I realize this doesn't work on heroku
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+
   get 'home/index'
 
   post '/signup' => 'home#signup'
